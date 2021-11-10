28a08c780c938851a89880935a7d0c58dc2586a4
sifelkateb <sifelkateb@gmail.com>
2021-11-10 17:17:07 +0200

    starter template

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import * as BooksAPI from './BooksAPI';
+import './App.css';
+
+
+
+class BooksApp extends React.Component {
+
+  render() {
+    return (
+      <div className="app">
+      <h1>Book App</h1>
+        
+      </div>
+    )
+  }
+}
+
+export default BooksApp
